6bd52edb9019f19a15ad9715034d1c46b4ed84f3
网友小宋 <103190643+xyzbz@users.noreply.github.com>
2025-02-16 12:54:48 +0800

    Update Plugin.php

--- LinkGo/Plugin.php
+++ LinkGo/Plugin.php
@@ -23,42 +23,59 @@ class LinkGo_Plugin implements Typecho_Plugin_Interface
         return '插件已禁用';
     }
 
-    public static function config(Typecho_Widget_Helper_Form $form) {}
+    public static function config(Typecho_Widget_Helper_Form $form)
+    {
+        // 无需白名单配置
+    }
 
     public static function personalConfig(Typecho_Widget_Helper_Form $form) {}
 
-    public static function convertLinks($content, $widget, $lastResult)
-    {
-        $content = empty($lastResult) ? $content : $lastResult;
-        $siteUrl = Typecho_Widget::widget('Widget_Options')->siteUrl;
-        return preg_replace_callback(
-            '/<\s*a\s+(.*?)href=([\'"])([^\'"]+)\2(.*?)>/i',
-            function ($matches) use ($siteUrl) {
-                $url = $matches[3];
-                if (strpos($url, $siteUrl) === false) {
-                    $encodedUrl = base64_encode($url);
-                    return '<a ' . $matches[1] . 'href="' . $siteUrl . 'go?target=' . $encodedUrl . '" target="_blank" ' . $matches[4] . '>';
-                } else {
-                    return '<a ' . $matches[1] . 'href="' . $url . '" ' . $matches[4] . '>';
-                }
-            },
-            $content
-        );
-    }
+public static function convertLinks($content, $widget, $lastResult)
+{
+    $content = empty($lastResult) ? $content : $lastResult;
+    $siteUrl = Typecho_Widget::widget('Widget_Options')->siteUrl;
 
+    return preg_replace_callback(
+        '/<\s*a\s+([^>]*?\s+)?href="([^"]*)"([^>]*)>(.*?)<\/a>/i', // 支持嵌套 HTML 结构
+        function ($matches) use ($siteUrl) {
+            $url = $matches[2]; // 捕获 href 属性的值
+            $attributes = $matches[1] . $matches[3]; // 捕获其他属性
+            $innerContent = $matches[4]; // 捕获 <a> 标签的内部内容
+            // 检查链接是否是本站链接
+            if (strpos($url, $siteUrl) === false) {
+                $encodedUrl = base64_encode($url);
+                // 强制添加 target="_blank" 属性
+                if (strpos($attributes, 'target=') === false) {
+                    $attributes .= ' target="_blank"';
+                }
+                return '<a href="' . $siteUrl . 'go?target=' . $encodedUrl . '"' . $attributes . '>' . $innerContent . '</a>';
+            } else {
+                return '<a href="' . $url . '"' . $attributes . '>' . $innerContent . '</a>';
+            }
+        },
+        $content
+    );
+}
     public static function convertCommentLinks($content, $widget, $lastResult)
     {
         $content = empty($lastResult) ? $content : $lastResult;
         $siteUrl = Typecho_Widget::widget('Widget_Options')->siteUrl;
+
         return preg_replace_callback(
-            '/<\s*a\s+(.*?)href=([\'"])([^\'"]+)\2(.*?)>/i',
+            '/<\s*a\s+(?:[^>]*?\s+)?href="([^"]*)"(.*?)>/i', // 支持复杂 <a> 标签
             function ($matches) use ($siteUrl) {
-                $url = $matches[3];
+                $url = $matches[1];
+                $attributes = $matches[2];
+                // 检查链接是否是本站链接
                 if (strpos($url, $siteUrl) === false) {
                     $encodedUrl = base64_encode($url);
-                    return '<a ' . $matches[1] . 'href="' . $siteUrl . 'go?target=' . $encodedUrl . '" target="_blank" ' . $matches[4] . '>';
+                    // 强制添加 target="_blank" 属性
+                    if (strpos($attributes, 'target=') === false) {
+                        $attributes .= ' target="_blank"';
+                    }
+                    return '<a href="' . $siteUrl . 'go?target=' . $encodedUrl . '"' . $attributes . '>';
                 } else {
-                    return '<a ' . $matches[1] . 'href="' . $url . '" ' . $matches[4] . '>';
+                    return '<a href="' . $url . '"' . $attributes . '>';
                 }
             },
             $content
@@ -69,6 +86,8 @@ class LinkGo_Plugin implements Typecho_Plugin_Interface
     {
         $siteUrl = Typecho_Widget::widget('Widget_Options')->siteUrl;
         $url = $comment['url'];
+
+        // 检查链接是否是本站链接
         if (strpos($url, $siteUrl) === false && !empty($url)) {
             $encodedUrl = base64_encode($url);
             $comment['url'] = $siteUrl . 'go?target=' . $encodedUrl . '" target="_blank';
